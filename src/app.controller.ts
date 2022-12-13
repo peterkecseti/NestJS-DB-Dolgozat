97bc9175aa1eb3ad6ff5d35e1e704c56b21ed38e
peterkecseti <peterkecseti@gmail.com>
2022-12-13 13:14:38 +0100

    kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import {
+  Body,
   Controller,
   Get,
+  Post,
+  Query,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import {MacskaDto} from './macska.dto'
 
 @Controller()
 export class AppController {
@@ -12,7 +17,31 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async listMacskak(){
+    const [rows] = await db.execute(
+      'SELECT szem_szin, suly FROM macskak ORDER BY suly DESC'
+    );
+    return {
+      macskak: rows,
+    };
+  }
+
+
+
+  @Get('macskak/new')
+  @Render('form')
+  newMacskaForm() {
+    return {};
+  }
+  @Post('macskak/new')
+  @Redirect()
+  async newMacska(@Body() macska: MacskaDto){
+    const [result]: any = await db.execute(
+      'INSERT INTO macskak (szem_szin, suly) VALUES (?, ?)',
+      [macska.szem_szin, macska.suly],
+    );
+    return {
+      url: '/',
+    };
   }
 }
---
+++ src/macska.dto.ts
@@ -0,0 +1,5 @@
+export class MacskaDto {
+    szem_szin: string;
+    suly: number;
+  }
+  
